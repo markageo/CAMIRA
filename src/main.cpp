67e066c774ff0721f8412e772c9dbe245a02e461
Mark <mark.george@sydney.edu.au>
2023-02-24 09:11:26 +1100

    Created basic directory structure

---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+/*---------------------------------------------------------------------------*\
+    3D coupled Navier-Stokes solver on rectilinear grids
+
+    Mark George
+\*---------------------------------------------------------------------------*/
+
+
+int main(int argc, char const *argv[]) 
+{
+
+    return 0;
+}
