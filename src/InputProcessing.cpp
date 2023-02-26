b34e43a5bcccd03544601eec9b8b0e3bd26491aa
mark <mark.george@sydney.edu.au>
2023-02-26 12:14:11 +1100

    Added new version of input parser which uses optional outputs

---
+++ src/InputProcessing.cpp
@@ -0,0 +1,20 @@
+#include "InputProcessing.h"
+
+#define NDEBUG
+
+#include "boost/property_tree/ptree.hpp"
+#include <string>
+#include <fstream>
+#include <iostream>
+#include <stack>
+#include <map>
+#include <cstdlib>
+#include <utility>
+
+
+namespace pt = boost::property_tree;
+
+/*-------------------------------------------------------------------------------------*\
+                                     Helper functions
+\*-------------------------------------------------------------------------------------*/
+
--- src/main.cpp
+++ src/main.cpp
@@ -7,5 +7,7 @@
 int main(int argc, char const *argv[]) 
 {
 
+    
+
     return 0;
 }
