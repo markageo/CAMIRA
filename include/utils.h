593a18e11449c8094d82d4eb5d99514a28266367
Mark <mark.george@sydney.edu.au>
2023-03-27 16:24:52 +1100

    writeArray is now a templated function in the utils.h header

--- include/utils.h
+++ include/utils.h
@@ -2,12 +2,44 @@
 #define UTILS
 
 #include "Types.h"
+#include "Tensor"
+
+#include <fstream>
+#include <iomanip>
+#include <iostream>
 
 namespace UTIL
 {
 
-// Write an an array to file in an easy to read way, used for debugging
-void writeArray(const std::string &, const CFD::array3D &, const int = 4);
+// Write eigen tensor to a file for debugging
+template<typename T>
+void writeArray(const std::string &filename, const T &array, const int precision = 4)
+{
+
+    std::ofstream fileStream(filename);
+    fileStream << std::fixed;
+    fileStream << std::setprecision(precision);
+
+    if (array.size() > 0) {
+
+        const auto& dims = array.dimensions();
+
+        if (dims.size() < 3) {
+
+            fileStream << array;
+
+        } else if (dims.size() == 3) {
+
+            for (int k = 0; k != dims[2]; k++) {
+                fileStream << array.chip(k , 2);
+                fileStream << "\n\n";
+            }
+
+        }
+
+    }
+
+}
 
 }
 
