23f9d7113539b139431db2461c62ef4953864552
Mark <mark.george@sydney.edu.au>
2023-04-05 16:05:15 +1000

    Face velocity calcution at boundaries now uses Eigen reductions instead of loops. This decreases code size.

--- src/FaceVelocities.cpp
+++ src/FaceVelocities.cpp
@@ -5,9 +5,7 @@
 namespace
 {
 
-// ------------------------------------------ Face velocity interpolation ------------------------------------------//
-
-void UpdateFaceVelocites_x(CFD::array3D &faceVel, const CFD::array3D &cellVel, const CFD::Mesh &mesh)
+void FaceVelocityXnormal(CFD::array3D &faceVel, const CFD::array3D &cellVel, const CFD::Mesh &mesh)
 {
     using namespace CFD;
     using enum Axis::ENUMDATA;
@@ -26,7 +24,7 @@ void UpdateFaceVelocites_x(CFD::array3D &faceVel, const CFD::array3D &cellVel, c
 }
 
 
-void UpdateFaceVelocites_y(CFD::array3D &faceVel, const CFD::array3D &cellVel, const CFD::Mesh &mesh)
+void FaceVelocityYnormal(CFD::array3D &faceVel, const CFD::array3D &cellVel, const CFD::Mesh &mesh)
 {
     using namespace CFD;
     using enum Axis::ENUMDATA;
@@ -45,7 +43,7 @@ void UpdateFaceVelocites_y(CFD::array3D &faceVel, const CFD::array3D &cellVel, c
 }
 
 
-void UpdateFaceVelocites_z(CFD::array3D &faceVel, const CFD::array3D &cellVel, const CFD::Mesh &mesh)
+void FaceVelocityZnormal(CFD::array3D &faceVel, const CFD::array3D &cellVel, const CFD::Mesh &mesh)
 {
     using namespace CFD;
     using enum Axis::ENUMDATA;
@@ -64,137 +62,7 @@ void UpdateFaceVelocites_z(CFD::array3D &faceVel, const CFD::array3D &cellVel, c
 }
 
 
-
-// ------------------------------------------ Boundary conditions on x normal faces ------------------------------------------ //
-
-void setZeroGradient_x( CFD::ArrayAllocator<CFD::Fields> &faceVelocities, const CFD::ArrayAllocator<CFD::Fields> &fields, 
-                        const CFD::iterType faceIndex, const CFD::iterType fieldIndex) 
-{
-    using namespace CFD;
-    using F = CFD::Fields::ENUMDATA;
-
-    for (iterType k = 0; k != faceVelocities[F::U].dimension(2); k++ ) {
-        for (iterType j = 0; j != faceVelocities[F::U].dimension(1); j++) {
-            faceVelocities[F::U](faceIndex, j, k) = fields[F::U](fieldIndex, j, k);
-        }
-    }   
-}
-
-
-void setUniform_x( CFD::ArrayAllocator<CFD::Fields> &faceVelocities, const CFD::iterType faceIndex, const CFD::floatType value) 
-{
-    using namespace CFD;
-    using F = CFD::Fields::ENUMDATA;
-
-    for (iterType k = 0; k != faceVelocities[F::U].dimension(2); k++ ) {
-        for (iterType j = 0; j != faceVelocities[F::U].dimension(1); j++) {
-            faceVelocities[F::U](faceIndex, j, k) = value;
-        }
-    }   
-}
-
-
-void setExtrapolated_x( CFD::ArrayAllocator<CFD::Fields> &faceVelocities, const CFD::ArrayAllocator<CFD::Fields> &fields, const CFD::Mesh::ExtrapFactorsStruct &extrapFactorsStruct, 
-                        const CFD::iterType faceIndex, const CFD::iterType fieldIndex_p, const CFD::iterType fieldIndex_f) 
-{
-    using namespace CFD;
-    using F = Fields::ENUMDATA;
-
-    for (iterType k = 0; k != faceVelocities[F::U].dimension(2); k++ ) {
-        for (iterType j = 0; j != faceVelocities[F::U].dimension(1); j++) {
-            faceVelocities[F::U](faceIndex, j, k) = extrapFactorsStruct.p * fields[F::U](fieldIndex_p, j, k)
-                                                  - extrapFactorsStruct.a * fields[F::U](fieldIndex_f, j, k);
-        }
-    } 
-}
-
-
-
-// ------------------------------------------ Boundary conditions on y normal faces ------------------------------------------ //
-
-void setZeroGradient_y( CFD::ArrayAllocator<CFD::Fields> &faceVelocities, const CFD::ArrayAllocator<CFD::Fields> &fields, 
-                        const CFD::iterType faceIndex, const CFD::iterType fieldIndex) 
-{
-    using namespace CFD;
-    using F = Fields::ENUMDATA;
-
-    for (iterType k = 0; k != faceVelocities[F::V].dimension(2); k++ ) {
-        for (iterType i = 0; i != faceVelocities[F::V].dimension(0); i++) {
-            faceVelocities[F::V](i, faceIndex, k) = fields[F::V](i, fieldIndex, k);
-        }
-    }   
-}
-
-void setUniform_y( CFD::ArrayAllocator<CFD::Fields> &faceVelocities, const CFD::iterType faceIndex, const CFD::floatType value) 
-{
-    using namespace CFD;
-    using F = Fields::ENUMDATA;
-
-    for (iterType k = 0; k != faceVelocities[F::V].dimension(2); k++ ) {
-        for (iterType i = 0; i != faceVelocities[F::V].dimension(0); i++) {
-            faceVelocities[F::V](i, faceIndex, k) = value;
-        }
-    }   
-}
-
-void setExtrapolated_y( CFD::ArrayAllocator<CFD::Fields> &faceVelocities, const CFD::ArrayAllocator<CFD::Fields> &fields, const CFD::Mesh::ExtrapFactorsStruct &extrapFactorsStruct, 
-                        const CFD::iterType faceIndex, const CFD::iterType fieldIndex_p, const CFD::iterType fieldIndex_f) 
-{
-    using namespace CFD;
-    using F = Fields::ENUMDATA;
-
-    for (iterType k = 0; k != faceVelocities[F::V].dimension(2); k++ ) {
-        for (iterType i = 0; i != faceVelocities[F::V].dimension(0); i++) {
-            faceVelocities[F::V](i, faceIndex, k) = extrapFactorsStruct.p * fields[F::V](i, fieldIndex_p, k)
-                                                  - extrapFactorsStruct.a * fields[F::V](i, fieldIndex_f, k);
-        }
-    } 
-}
-
-
-// ------------------------------------------ Boundary conditions on z normal faces ------------------------------------------ //
-
-void setZeroGradient_z( CFD::ArrayAllocator<CFD::Fields> &faceVelocities, const CFD::ArrayAllocator<CFD::Fields> &fields, 
-                        const CFD::iterType faceIndex, const CFD::iterType fieldIndex) 
-{
-    using namespace CFD;
-    using F = Fields::ENUMDATA;
-
-    for (iterType j = 0; j != faceVelocities[F::W].dimension(1); j++ ) {
-        for (iterType i = 0; i != faceVelocities[F::W].dimension(0); i++) {
-            faceVelocities[F::W](i, j, faceIndex) = fields[F::W](i, j, fieldIndex);
-        }
-    }   
-}
-
-void setUniform_z( CFD::ArrayAllocator<CFD::Fields> &faceVelocities, const CFD::iterType faceIndex, const CFD::floatType value) 
-{
-    using namespace CFD;
-    using F = Fields::ENUMDATA;
-
-    for (iterType j = 0; j != faceVelocities[F::W].dimension(1); j++ ) {
-        for (iterType i = 0; i != faceVelocities[F::W].dimension(0); i++) {
-            faceVelocities[F::W](i, j, faceIndex) = value;
-        }
-    }   
-}
-
-void setExtrapolated_z( CFD::ArrayAllocator<CFD::Fields> &faceVelocities, const CFD::ArrayAllocator<CFD::Fields> &fields, const CFD::Mesh::ExtrapFactorsStruct &extrapFactorsStruct, 
-                        const CFD::iterType faceIndex, const CFD::iterType fieldIndex_p, const CFD::iterType fieldIndex_f) 
-{
-    using namespace CFD;
-    using F = Fields::ENUMDATA;
-   
-    for (iterType j = 0; j != faceVelocities[F::W].dimension(1); j++ ) {
-        for (iterType i = 0; i != faceVelocities[F::W].dimension(0); i++) {
-            faceVelocities[F::W](i, j, faceIndex) = extrapFactorsStruct.p * fields[F::W](i, j, fieldIndex_p)
-                                                  - extrapFactorsStruct.a * fields[F::W](i, j, fieldIndex_f);
-        }
-    } 
-}
-
 }   // end anonymous namespace
-
 
 
 namespace CFD
@@ -205,140 +73,74 @@ void UpdateFaceVelocities( ArrayAllocator<Fields> &faceVelocities, const Mesh &m
 {
     using F = Fields::ENUMDATA;
     using BC = BoundaryConditions::ENUMDATA;
-    using BP = BoundaryPatches::ENUMDATA;
+    using enum Axis::ENUMDATA;
 
     // Non-boundary cells
-    UpdateFaceVelocites_x( faceVelocities[F::U], fields[F::U], mesh);
-    UpdateFaceVelocites_y( faceVelocities[F::V], fields[F::V], mesh);
-    UpdateFaceVelocites_z( faceVelocities[F::W], fields[F::W], mesh);
+    FaceVelocityXnormal( faceVelocities[F::U], fields[F::U], mesh);
+    FaceVelocityYnormal( faceVelocities[F::V], fields[F::V], mesh);
+    FaceVelocityZnormal( faceVelocities[F::W], fields[F::W], mesh);
 
     // Just to assign the boundary index of the face flux and field arrays
+    constexpr std::array<Fields::ENUMDATA, 3> faceVelocityFields = {F::U, F::V, F::W};
+    Fields::ENUMDATA axisVel;
     iterType faceEndIndex, fieldEndIndex;
+    floatType extrapFactor_p, extrapFactor_a;
+    BoundaryPatches::ENUMDATA positivePatch, negativePatch;
 
-    // +x boundary
-    faceEndIndex = faceVelocities[F::U].dimension(0)-1;
-    fieldEndIndex = fields[F::U].dimension(0)-1;
-    switch ( boundaryConditions[F::U][BP::xPositive].type ) {
-        
-        case BC::zeroGradient:
-            setZeroGradient_x(faceVelocities, fields, faceEndIndex, fieldEndIndex);          
-            break;
+    for (int axis = 0; axis != Axis::count; axis++) {
 
-        case BC::uniform:
-            setUniform_x(faceVelocities, faceEndIndex, boundaryConditions[F::U][BP::xPositive].value);
-            break;
+        positivePatch = positivePatches[axis];
+        negativePatch = negativePatches[axis];
+        axisVel = faceVelocityFields[axis];
 
-        case BC::extrapolated:
-            setExtrapolated_x(faceVelocities, fields, mesh.extrapFactors[BP::xPositive], faceEndIndex, fieldEndIndex, fieldEndIndex-1);
-            break;
+        // Axis positive boundary
+        faceEndIndex = faceVelocities[axisVel].dimension(axis)-1;
+        fieldEndIndex = fields[axisVel].dimension(axis)-1;
+        switch ( boundaryConditions[axisVel][positivePatch].type ) {
+            
+            case BC::zeroGradient:
+                faceVelocities[axisVel].chip(faceEndIndex, axis) = fields[axisVel].chip(fieldEndIndex, axis);          
+                break;
 
-        default:
-            break;
-    }
+            case BC::uniform:
+                faceVelocities[axisVel].chip(faceEndIndex, axis) = faceVelocities[axisVel].chip(faceEndIndex, axis).constant( boundaryConditions[axisVel][positivePatch].value );
+                break;
 
+            case BC::extrapolated:
+                extrapFactor_p = mesh.extrapFactors[positivePatch].p;
+                extrapFactor_a = mesh.extrapFactors[positivePatch].a;
+                faceVelocities[axisVel].chip(faceEndIndex, axis) = fields[axisVel].chip(fieldEndIndex, axis) * fields[axisVel].chip(fieldEndIndex, axis).constant( extrapFactor_p )
+                                                                 + fields[axisVel].chip(fieldEndIndex-1, axis) * fields[axisVel].chip(fieldEndIndex-1, axis).constant( extrapFactor_a );
+                break;
 
-    // -x boundary
-    faceEndIndex = 0;
-    fieldEndIndex = 0;
-    switch ( boundaryConditions[F::U][BP::xNegative].type ) {
-        case BC::zeroGradient:
-            setZeroGradient_x(faceVelocities, fields, faceEndIndex, fieldEndIndex); 
-            break;
-
-        case BC::uniform:
-            setUniform_x(faceVelocities, faceEndIndex, boundaryConditions[F::U][BP::xNegative].value);
-            break;
-
-        case BC::extrapolated:
-            setExtrapolated_x(faceVelocities, fields, mesh.extrapFactors[BP::xNegative], faceEndIndex, fieldEndIndex, fieldEndIndex+1);
-            break;
-
-        default:
-            break;
-    }
+            default:
+                break;
+        }
 
 
-    // +y boundary
-    faceEndIndex = faceVelocities[F::V].dimension(1)-1;
-    fieldEndIndex = fields[F::V].dimension(1)-1;
-    switch ( boundaryConditions[F::V][BP::yPositive].type ) {
-        case BC::zeroGradient:
-            setZeroGradient_y(faceVelocities, fields, faceEndIndex, fieldEndIndex); 
-            break;
+        // Axis negative boundary
+        faceEndIndex = 0;
+        fieldEndIndex = 0;
+        switch ( boundaryConditions[axisVel][negativePatch].type ) {
+            
+            case BC::zeroGradient:
+                faceVelocities[axisVel].chip(faceEndIndex, axis) = fields[axisVel].chip(fieldEndIndex, axis);          
+                break;
 
-        case BC::uniform:
-            setUniform_y(faceVelocities, faceEndIndex, boundaryConditions[F::V][BP::yPositive].value);
-            break;
+            case BC::uniform:
+                faceVelocities[axisVel].chip(faceEndIndex, axis) = faceVelocities[axisVel].chip(faceEndIndex, axis).constant( boundaryConditions[axisVel][negativePatch].value );
+                break;
 
-        case BC::extrapolated:
-            setExtrapolated_y(faceVelocities, fields, mesh.extrapFactors[BP::yPositive], faceEndIndex, fieldEndIndex, fieldEndIndex-1);
-            break;
+            case BC::extrapolated:
+                extrapFactor_p = mesh.extrapFactors[negativePatch].p;
+                extrapFactor_a = mesh.extrapFactors[negativePatch].a;
+                faceVelocities[axisVel].chip(faceEndIndex, axis) = fields[axisVel].chip(fieldEndIndex, axis) * fields[axisVel].chip(fieldEndIndex, axis).constant( extrapFactor_p )
+                                                                 + fields[axisVel].chip(fieldEndIndex+1, axis) * fields[axisVel].chip(fieldEndIndex-1, axis).constant( extrapFactor_a );
+                break;
 
-        default:
-            break;
-    }
-
-
-    // -y boundary
-    faceEndIndex = 0;
-    fieldEndIndex = 0;
-    switch ( boundaryConditions[F::V][BP::yNegative].type ) {
-        case BC::zeroGradient:
-            setZeroGradient_y(faceVelocities, fields, faceEndIndex, fieldEndIndex); 
-            break;
-
-        case BC::uniform:
-            setUniform_y(faceVelocities, faceEndIndex, boundaryConditions[F::V][BP::yNegative].value);
-            break;
-
-        case BC::extrapolated:
-            setExtrapolated_y(faceVelocities, fields, mesh.extrapFactors[BP::yNegative], faceEndIndex, fieldEndIndex, fieldEndIndex+1);
-            break;
-
-        default:
-            break;
-    }
-
-
-    // +z boundary
-    faceEndIndex = faceVelocities[F::W].dimension(2)-1;
-    fieldEndIndex = fields[F::W].dimension(2)-1;
-    switch ( boundaryConditions[F::W][BP::zPositive].type ) {
-        case BC::zeroGradient:
-            setZeroGradient_z(faceVelocities, fields, faceEndIndex, fieldEndIndex); 
-            break;
-
-        case BC::uniform:
-            setUniform_z(faceVelocities, faceEndIndex, boundaryConditions[F::W][BP::zPositive].value);
-            break;
-
-        case BC::extrapolated:
-            setExtrapolated_z(faceVelocities, fields, mesh.extrapFactors[BP::zPositive], faceEndIndex, fieldEndIndex, fieldEndIndex-1);
-            break;
-
-        default:
-            break;
-    }
-
-
-    // -z boundary
-    faceEndIndex = 0;
-    fieldEndIndex = 0;
-    switch ( boundaryConditions[F::W][BP::zNegative].type ) {
-        case BC::zeroGradient:
-            setZeroGradient_z(faceVelocities, fields, faceEndIndex, fieldEndIndex); 
-            break;
-
-        case BC::uniform:
-            setUniform_z(faceVelocities, faceEndIndex, boundaryConditions[F::W][BP::zNegative].value);
-            break;
-
-        case BC::extrapolated:
-            setExtrapolated_z(faceVelocities, fields, mesh.extrapFactors[BP::zNegative], faceEndIndex, fieldEndIndex, fieldEndIndex+1);
-            break;
-
-        default:
-            break;
+            default:
+                break;
+        }
     }
 
 }
