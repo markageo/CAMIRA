7d38858135ba1506ca1d60d2eccf0611ef72dd5a
Mark <mark.george@sydney.edu.au>
2023-04-24 11:57:34 +1000

    Testing function to compare reference mesh to outputted mesh

--- tests/TestFunctions.cpp
+++ tests/TestFunctions.cpp
@@ -65,7 +65,7 @@ namespace
         return TEST::none;
     }
 
-}
+}   // end anonymous namespace
 
 
 TEST::TestConfig TEST::ReadConfig(const std::string &filename)
@@ -108,7 +108,7 @@ TEST::TestConfig TEST::ReadConfig(const std::string &filename)
 }
 
 
-void TEST::WriteMesh(const CFD::Mesh &mesh, const std::string &filedir) 
+void TEST::WriteMesh(const CFD::Mesh &mesh, const std::string &filedir, const int precision) 
 {
 
     using AX = CFD::Axis::ENUMDATA;
@@ -129,39 +129,147 @@ void TEST::WriteMesh(const CFD::Mesh &mesh, const std::string &filedir)
         axis = static_cast<AX>(a);
 
         // Cell Lengths
-        UTIL::WriteArray(Fname("cell_lengths"), mesh.cellLengths[axis]);   
+        UTIL::WriteArray(Fname("cell_lengths"), mesh.cellLengths[axis], precision);   
 
         // Inverse cell lengths
-        UTIL::WriteArray(Fname("inv_cell_lengths"), mesh.cellLengthsInv[axis]);   
+        UTIL::WriteArray(Fname("inv_cell_lengths"), mesh.cellLengthsInv[axis], precision);   
 
         // Cell centers
-        UTIL::WriteArray(Fname("cell_centers"), mesh.cellCenters[axis]);  
+        UTIL::WriteArray(Fname("cell_centers"), mesh.cellCenters[axis], precision);  
 
         // Inverse of cell center difference
-        UTIL::WriteArray(Fname("inv_cell_center_diff"), mesh.cellCenterDiffInv[axis]);
+        UTIL::WriteArray(Fname("inv_cell_center_diff"), mesh.cellCenterDiffInv[axis], precision);
 
         // Cell faces
-        UTIL::WriteArray(Fname("cell_faces"), mesh.cellFaces[axis]);
+        UTIL::WriteArray(Fname("cell_faces"), mesh.cellFaces[axis], precision);
 
         // Write interpolation factors to a file
-        UTIL::WriteArray(Fname("interp_factors"), mesh.interpFactors[axis]);
+        UTIL::WriteArray(Fname("interp_factors"), mesh.interpFactors[axis], precision);
 
         // Put the extrapolation factors in an array and write it out
         extrapFactorsArray(0, 0) = mesh.extrapFactors[CFD::positivePatches[axis]].p;
         extrapFactorsArray(1, 0) = mesh.extrapFactors[CFD::positivePatches[axis]].a;
         extrapFactorsArray(0, 1) = mesh.extrapFactors[CFD::negativePatches[axis]].p;
         extrapFactorsArray(1, 1) = mesh.extrapFactors[CFD::negativePatches[axis]].a;
-        UTIL::WriteArray(Fname("extrap_factors"), extrapFactorsArray);
+        UTIL::WriteArray(Fname("extrap_factors"), extrapFactorsArray, precision);
 
     }
 
 }
 
 
-bool TEST::CompareMesh(const CFD::Mesh &, const std::string &)
+// Helper functions Comparision tests
+namespace 
 {
 
+    // Returns true if all elements of a tensor are exactly the same
+    template<typename T>
+    bool ArraysSame(const T &array1, const T &array2) 
+    {
+        using dimType = long int;
 
-    return false;
+        // Check their dimensions are the same
+        if (array1.size() != array2.size())
+            return false;
+
+        // Loop through and compare
+        dimType n = array1.size();
+        auto *array1Data = array1.data();
+        auto *array2Data = array2.data();
+        for (dimType i = 0; i != n; i++) {
+            if ( array1Data[i] != array2Data[i] )
+                return false;
+        }
+        return true;
+    }
+
+
+    // Read and compare arrays. Returns true if they are the same
+    template<typename T>
+    bool ReadAndCompare(const std::string &filename1, const std::string &filename2) {
+
+        // Temporaries for storing read in data
+        T array1, array2;
+        array1 = UTIL::ReadArray<CFD::array1D>(filename1);
+        array2 = UTIL::ReadArray<CFD::array1D>(filename2);
+        if (!ArraysSame(array1, array2))
+            return false;
+
+        return true;
+
+    }
+
+
+}   // end anonymous namespace
+
+bool TEST::CompareMesh(const std::string &outputDir, const std::string &referenceDir)
+{
+
+    using AX = CFD::Axis::ENUMDATA;
+
+    AX axis;
+    std::string ext = ".dat";
+    CFD::EnumVector<CFD::Axis, std::string> axisSuffix{ {"_x", "_y", "_z"} };
+    std::string fname;
+    auto Fname = [&] (const std::string &name, const std::string &dir) -> std::string { return dir + name + axisSuffix[axis] + ext; };
+
+    // Temporary array for extrapolation factors
+    // rows  : extrapFactors.p, extrapFactors.a
+    // cols  : positivePatch, negativepatch
+    CFD::array2D extrapFactorsArray(2, 2); 
+
+    // Temporary variable for output and reference directories
+    CFD::array1D outputArray1D, referenceArray1D;
+    CFD::array2D outputArray2D, referenceArray2D;
+
+    for (int a = 0; a != 3; a++) {
+        axis = static_cast<AX>(a);
+
+        // Cell Lengths
+        outputArray1D = UTIL::ReadArray<CFD::array1D>(Fname("cell_lengths", outputDir));
+        referenceArray1D = UTIL::ReadArray<CFD::array1D>(Fname("cell_lengths", referenceDir));
+        if (!ArraysSame(outputArray1D, referenceArray1D))
+            return false;
+
+        // Inverse cell lengths
+        outputArray1D = UTIL::ReadArray<CFD::array1D>(Fname("inv_cell_lengths", outputDir));
+        referenceArray1D = UTIL::ReadArray<CFD::array1D>(Fname("inv_cell_lengths", referenceDir));
+        if (!ArraysSame(outputArray1D, referenceArray1D))
+            return false;  
+
+        // Cell centers
+        outputArray1D = UTIL::ReadArray<CFD::array1D>(Fname("cell_centers", outputDir));
+        referenceArray1D = UTIL::ReadArray<CFD::array1D>(Fname("cell_centers", referenceDir));
+        if (!ArraysSame(outputArray1D, referenceArray1D))
+            return false;
+
+        // Inverse of cell center difference
+        outputArray1D = UTIL::ReadArray<CFD::array1D>(Fname("inv_cell_center_diff", outputDir));
+        referenceArray1D = UTIL::ReadArray<CFD::array1D>(Fname("inv_cell_center_diff", referenceDir));
+        if (!ArraysSame(outputArray1D, referenceArray1D))
+            return false;
+
+        // Cell faces
+        outputArray1D = UTIL::ReadArray<CFD::array1D>(Fname("cell_faces", outputDir));
+        referenceArray1D = UTIL::ReadArray<CFD::array1D>(Fname("cell_faces", referenceDir));
+        if (!ArraysSame(outputArray1D, referenceArray1D))
+            return false;
+
+        // Write interpolation factors to a file
+        outputArray1D = UTIL::ReadArray<CFD::array1D>(Fname("interp_factors", outputDir));
+        referenceArray1D = UTIL::ReadArray<CFD::array1D>(Fname("interp_factors", referenceDir));
+        if (!ArraysSame(outputArray1D, referenceArray1D))
+            return false;
+
+        // Put the extrapolation factors in an array and write it out
+        outputArray2D = UTIL::ReadArray<CFD::array2D>(Fname("extrap_factors", outputDir));
+        referenceArray2D = UTIL::ReadArray<CFD::array2D>(Fname("extrap_factors", referenceDir));
+        if (!ArraysSame(outputArray2D, referenceArray2D))
+            return false;
+
+
+    }
+
+    return true;
 
 }
