0d3b6c29f4decaa112844d03f269b26a052f1ad8
Mark <mark.george@sydney.edu.au>
2023-04-26 14:40:33 +1000

    Testing now writes finite volume coefficients to output directory

--- tests/TestFunctions.h
+++ tests/TestFunctions.h
@@ -25,13 +25,21 @@ struct TestConfig
     std::string meshTestOutputDirectory;
     std::string meshTestReferenceDirectory;
 
+    // General testing that involves face velocities
+    std::string velUniformTestInputFilename;
+    std::string velZeroGradientTestInputFilename;
+    std::string velExtrapolatedTestInputFilename;
+
     // Face velocity testing
-    std::string faceVelUniformTestInputFilename;
-    std::string faceVelZeroGradientTestInputFilename;
-    std::string faceVelExtrapolatedTestInputFilename;
     testState faceVelTest;
     std::string faceVelTestOutputDirectory;
     std::string faceVelTestReferenceDirectory;
+
+    // Finite volume coefficient testing
+    testState fvCoeffTest;
+    std::string fvCoeffTestOutputDirectory;
+    std::string fvCoeffTestReferenceDirectory;
+
 };
 
 // Read test config files
@@ -42,10 +50,12 @@ TestConfig ReadConfig(const std::string &);
 void WriteMesh(const CFD::Mesh &, const std::string &, const int = 6);
 void WriteFields(const CFD::ArrayAllocator<CFD::Fields, CFD::array3D> &, const std::string &, const int = 6);
 void WriteFaceVels(const CFD::EnumVector<CFD::BoundaryConditions, CFD::ArrayAllocator<CFD::Fields, CFD::array3D> > &, const std::string &, const int = 6);
+void WriteFVCoeffs(const CFD::EnumVector<CFD::BoundaryConditions, CFD::FVCoefficients > &, const std::string &, const int = 6);
 
 // Compare data in two different directories
 bool CompareMesh(const std::string &, const std::string &);
 bool CompareFaceVels(const std::string &, const std::string &);
+bool CompareFVCoeffs(const std::string &, const std::string &);
 
 
 }   // end namespace TEST
