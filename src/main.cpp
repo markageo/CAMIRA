f53234f3c6330fea44d198112b51112782e0f8c4
Mark <mark.george@sydney.edu.au>
2023-06-28 09:38:49 +1000

    Refactored post solution transformation code

--- src/main.cpp
+++ src/main.cpp
@@ -52,7 +52,7 @@ int main(int argc, char const *argv[])
     \*-------------------------------------------------------------------------------------*/
 
     // // Undo the boundary condition transformation
-    // CFD::TransformToUserCoordinates(mesh, fields, axisTransformation);
+    CFD::TransformToUserCoordinates(mesh, fields, axisTransformation);
 
     // // Remove ghost cells from the fields
     // CFD::EnumFor<CFD::Fields>([&](CFD::Fields::ENUMDATA field)
