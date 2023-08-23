73e19823c08c3668ca5ff83742588d757b5d770d
Mark <mark.george@sydney.edu.au>
2023-08-23 14:58:21 +1000

    Main skeleton of immersed boundary data creation. Need to impliment function for determining nearest fluid nodes

--- src/ImmersedBoundary/ImmersedBoundaryData.cpp
+++ src/ImmersedBoundary/ImmersedBoundaryData.cpp
@@ -1,13 +1,77 @@
 #include "ImmersedBoundary.h"
 
+#include <CGAL/Simple_cartesian.h>
+#include <CGAL/Polyhedron_3.h>
+#include <CGAL/AABB_tree.h>
+#include <CGAL/AABB_traits.h>
+#include <CGAL/boost/graph/graph_traits_Polyhedron_3.h>
+#include <CGAL/AABB_face_graph_triangle_primitive.h>
+#include <CGAL/algorithm.h>
+#include <CGAL/Side_of_triangle_mesh.h>
+
 
 namespace CFD
 {
 
-
 namespace 
 {
 
+// Find closes point on surface of polyhedron to the query point
+fVector3 ClosestPoint( const Polyhedron &polyhedron, 
+                       const fVector3 &queryPoint) 
+{
+    using Point        = Polyhedron::Point_3;
+    using Primitive    = CGAL::AABB_face_graph_triangle_primitive<Polyhedron>;
+    using Traits       = CGAL::AABB_traits<CGAL_Kernel, Primitive>;
+    using Tree         = CGAL::AABB_tree<Traits>;
+    using Point_inside = CGAL::Side_of_triangle_mesh<Polyhedron, CGAL_Kernel>;
+
+    // Construct AABB tree with a KdTree
+    Tree tree(faces(polyhedron).first, faces(polyhedron).second, polyhedron);
+    tree.accelerate_distance_queries();
+
+    Point closest_point = tree.closest_point( Point( queryPoint(0), queryPoint(1), queryPoint(2)) );
+    return { closest_point[0], closest_point[1], closest_point[2] };
+}
+
+
+
+// Return index of nearest fluid cells to query point
+std::array< TensorIndex3D, IBGhostCell::numInterpPoints > NearestFluidCellIndices( const fVector3 &queryPoint, 
+                                                                                   const CellIDTensor3D &cellID,
+                                                                                   const Mesh &mesh, 
+                                                                                   const intType numPoints )
+{
+    std::array< TensorIndex3D, IBGhostCell::numInterpPoints > nearestPointIndices( numPoints );
+
+    // TODO
+
+    return nearestPointIndices;
+}
+
+
+IBGhostCell::InterpMatrix GetPointsMatrixInv( const std::array< TensorIndex3D, IBGhostCell::numInterpPoints > &fluidCellIndices,
+                                              const fVector3 &boundaryPoint, 
+                                              const Mesh &mesh)
+{
+    using enum Axis::ENUMDATA;
+    using MatrixRow = Eigen::Matrix<floatType, IBGhostCell::numInterpPoints, IBGhostCell::numInterpPoints>;
+    IBGhostCell::InterpMatrix pointsMatrixInv;
+
+    // First row is the boundary point
+    pointsMatrixInv.row(0) = MatrixRow{1.0f, boundaryPoint(0), boundaryPoint(1), boundaryPoint(2)};
+
+    for ( intType i = 1; i != IBGhostCell::numInterpPoints; i++ ) {
+
+        floatType xf = mesh.cellCenters[X]( fluidCellIndices[i][X] ),
+                  yf = mesh.cellCenters[Y]( fluidCellIndices[i][Y] ),
+                  zf = mesh.cellCenters[Z]( fluidCellIndices[i][Z] );
+
+         pointsMatrixInv.row(i) = MatrixRow{1.0f, xf, yf, zf};
+    }
+
+    return pointsMatrixInv.inverse();
+}
 
 
 }   // end anonymous namespace
@@ -16,7 +80,8 @@ namespace
 
 
 
-IBData CreateImmersedBoundaryData( const CellIDTensor3D &cellID, 
+IBData CreateImmersedBoundaryData( const Polyhedron &geometry,
+                                   const CellIDTensor3D &cellID, 
                                    const Mesh &mesh )
 {
 
@@ -37,17 +102,28 @@ IBData CreateImmersedBoundaryData( const CellIDTensor3D &cellID,
                     continue;
                 }
 
-                // Find coordinates of nearest immersed boundary point
+                fVector3 ghostPoint = { mesh.cellCenters[X](i), 
+                                        mesh.cellCenters[Y](j),
+                                        mesh.cellCenters[Z](k) };
+                TensorIndex3D ghostPointIndex = {i, j, k};
 
+                // Find coordinates of nearest immersed boundary point
+                fVector3 boundaryPoint = ClosestPoint( geometry, ghostPoint );
 
                 // Get the normal vector
+                fVector3 normalVector = boundaryPoint - ghostPoint;
 
                 // Determine coordinates of image point
+                fVector3 imagePoint = boundaryPoint + normalVector;
 
                 // Find nearest fluid cells for interpolation
+                std::array< TensorIndex3D, IBGhostCell::numInterpPoints > fluidCellIndices = NearestFluidCellIndices( boundaryPoint, cellID, mesh, IBGhostCell::numInterpPoints );
 
                 // Form the interpolation coefficient matrix
+                IBGhostCell::InterpMatrix pointsMatrixInv = GetPointsMatrixInv( fluidCellIndices, boundaryPoint, mesh );
 
+                // Fill up the ghost cell data struct
+                ibData.ghostCells.push_back( { fluidCellIndices, ghostPointIndex, pointsMatrixInv } );
 
             }
         }
