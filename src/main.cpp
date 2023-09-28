cde4eb1aa743a64115092c4bd0fadaeb0c2c4aae
Mark <mark.george@sydney.edu.au>
2023-09-28 15:01:41 +1000

    Profiling information is now written to file

--- src/main.cpp
+++ src/main.cpp
@@ -85,6 +85,9 @@ int main(int argc, char const *argv[])
     // Display profiling info to terminal and write to file
     #ifdef CFD_PROFILING
         std::cout << PROF::prof;
+
+        std::ofstream profilingFilestream( inputData.profilingFilename );
+        profilingFilestream << PROF::prof;
     #endif
     
     return 0;
