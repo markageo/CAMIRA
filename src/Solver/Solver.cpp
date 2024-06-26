d34ea9ab1286b6916fe5e4e06d5a9460b4132fa7
Mark <mark.george@sydney.edu.au>
2024-06-26 10:20:00 +1000

    Residuals for iteration zero written to file

--- src/Solver/Solver.cpp
+++ src/Solver/Solver.cpp
@@ -474,6 +474,7 @@ void SweepSolve( const InputData &inputData,
     TIC("Solver Loop")
     consoleLog.WriteHeader();
     consoleLog.WriteResiduals( residualsOuter, massFluxResidual, 0 );
+    residualsLogFile.WriteData( residualsOuter, massFluxResidual, 0 );
     for ( intType nOuterIterations = 1; nOuterIterations <= maxOuterIterations; nOuterIterations++ )
     {
         TIC("Multigrid Cycling")
