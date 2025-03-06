26bc253210fea09d31824dbf699e30134a4db6ac
Mark <mark.george@sydney.edu.au>
2025-03-06 15:45:42 +1100

    Changed Sub2Ind function to use simple loops instead of fold expressions, this is easier to read, and it was checked in compiler explorer that they are compile time unrolled anyway.

--- src/Solver/ArrayIndexConversions.h
+++ src/Solver/ArrayIndexConversions.h
@@ -2,46 +2,50 @@
 #define INDEX_CONVERSIONS
 
 #include "../Core/Types.h"
+#include <utility>
 
 namespace CFD
 {
 
-// Convert list of subscript array indices into linear 
+
+// Convert list of subscript array indices into linear index
 template< int ndims,
           Eigen::StorageOptions layout = Eigen::ColMajor, 
           class ...Args > 
 __attribute__((always_inline)) 
-inline intType Sub2Ind( const Eigen::Array<intType, ndims, 1> &dims, Args... args )
+inline constexpr intType Sub2Ind( const Eigen::Array<intType, ndims, 1> &dims, Args... args )
 {
     static_assert( ndims == sizeof...(Args) );
     static_assert( layout == Eigen::ColMajor || 
                    layout == Eigen::RowMajor  );
 
+    std::array<std::common_type_t<Args...>, ndims> indices = {args...};
+
     intType idx = 0;
     if constexpr ( layout == Eigen::ColMajor ) {
 
-        // idx = 0;
-        // idx = idx * m_dims[ndims - 1] + args[ndims - 1]
-        // idx = idx * m_dims[ndims - 2] + args[ndims - 2]
-        // idx = idx * m_dims[ndims - 3] + args[ndims - 3]
-        //              :
-        //              :
-        // idx = idx * m_dims[0] + args[0]
-        intType i = ndims-1;
-        intType dummy;
-        ( dummy = ... = (idx = idx*dims[i--] + args ) );  // Trick to evaluate fold expression in reverse
+        for ( intType i = 0; i != ndims; i++ ) {
+
+            intType stride = 1;
+            for ( intType j = 0; j != i; j++ ) {
+                stride *= dims(j);
+            }
+
+            idx += stride * indices[i];
+        }
 
     } else if constexpr ( layout == Eigen::RowMajor ) {
 
-        // idx = 0;
-        // idx = idx * m_dims[0] + args[0]
-        // idx = idx * m_dims[1] + args[1]
-        // idx = idx * m_dims[2] + args[2]
-        //              :
-        //              :
-        // idx = idx * m_dims[ndims - 1] + args[ndims - 1]
-        intType i = 0;
-        ( ... , (idx = idx*dims[i++] + args ));
+        for ( intType i = 0; i != ndims; i++ ) {
+
+            intType stride = 1;
+            for ( intType j = ndims; j > i; j-- ) {
+                stride *= dims(j);
+            }
+
+            idx += stride * indices[i];
+        }
+
     }
 
     return idx;
@@ -51,11 +55,10 @@ inline intType Sub2Ind( const Eigen::Array<intType, ndims, 1> &dims, Args... arg
 
 
 // Convert linear index into list of subscripts
-// TODO: Optimise this so that the loops are unrolled at compile time e.g. use fold expressions like above
 template< int ndims,
           Eigen::StorageOptions layout = Eigen::ColMajor > 
 __attribute__((always_inline)) 
-Eigen::array<Eigen::Index, ndims> Ind2Sub( const Eigen::Array<intType, ndims, 1> &dims, intType idx )
+inline constexpr Eigen::array<Eigen::Index, ndims> Ind2Sub( const Eigen::Array<intType, ndims, 1> &dims, intType idx )
 {
     static_assert( layout == Eigen::ColMajor || 
                    layout == Eigen::RowMajor  );
