49f4eb70a64d5bbe69ee558a39e2c20b390c7edb
Mark <mark.george@sydney.edu.au>
2025-05-01 13:50:03 +1000

    input file options now allow to choose between serial and parallel smoothers

--- src/Multigrid/Multigrid.cpp
+++ src/Multigrid/Multigrid.cpp
@@ -106,18 +106,22 @@ void SetMGLevels( std::vector< GridLevelData<MI > > &mgLevels,
                                                                      mgl.mesh.nCells(1) + 2*nGhost, 
                                                                      mgl.mesh.nCells(2) + 2*nGhost).setZero() );
 
-        // // Linear Solver
-        // switch ( inputData.smootherSettings.type ) {
-        //     case Smoothers::nestedLineSymmetric:
-        //         mgl.linearSolver = std::make_unique< nestedLineSymmetricSolverSerial<MI> >(mgl.fields, mgl.fieldsOld, mgl.ibData.mask, mgl.fvCoeffs, mgl.mesh, mgl.bcData, inputData.smootherSettings);
-        //         break;
+        // Linear Solver
+        switch ( inputData.smootherSettings.type ) {
+            case Smoothers::nestedLineSymmetricSerial:
+                mgl.linearSolver = std::make_unique< nestedLineSymmetricSolverSerial<MI> >(mgl.fields, mgl.fieldsOld, mgl.ibData.mask, mgl.fvCoeffs, mgl.mesh, mgl.bcData, inputData.smootherSettings);
+                break;
             
-        //     case Smoothers::domainSymmetric:
-        //         mgl.linearSolver = std::make_unique< domainSymmetricSolverSerial<MI> >(mgl.fields, mgl.fieldsOld, mgl.ibData.mask, mgl.fvCoeffs, mgl.mesh, mgl.bcData, inputData.smootherSettings);
-        //         break;
-        // }
+            case Smoothers::domainSymmetricSerial:
+                mgl.linearSolver = std::make_unique< domainSymmetricSolverSerial<MI> >(mgl.fields, mgl.fieldsOld, mgl.ibData.mask, mgl.fvCoeffs, mgl.mesh, mgl.bcData, inputData.smootherSettings);
+                break;
 
-        mgl.linearSolver = std::make_unique< domainSymmetricSolverParallel<MI> >(mgl.fields, mgl.fieldsOld, mgl.ibData.mask, mgl.fvCoeffs, mgl.mesh, mgl.bcData, inputData.smootherSettings);
+            case Smoothers::domainSymmetricParallel:
+                mgl.linearSolver = std::make_unique< domainSymmetricSolverParallel<MI> >(mgl.fields, mgl.fieldsOld, mgl.ibData.mask, mgl.fvCoeffs, mgl.mesh, mgl.bcData, inputData.smootherSettings);
+                break;
+        }
+
+        
         
 
     }
