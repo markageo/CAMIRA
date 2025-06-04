648a5a27a12ed811955750a4e85b8611b484a468
Mark <mark.george@sydney.edu.au>
2025-06-04 16:52:14 +1000

    Added RAJA, Umpire, camp, and blt submodules. Modified CMake to use blt to include these dependencies. Added updated versions of GPU compatible arrays and EnumVector

--- src/Core/Types.h
+++ src/Core/Types.h
@@ -7,6 +7,11 @@
 #include <CGAL/Simple_cartesian.h>
 #include <CGAL/Polyhedron_3.h>
 
+#include "RAJA/RAJA.hpp"
+#include "umpire/Allocator.hpp"
+#include "umpire/ResourceManager.hpp"
+#include "umpire/Umpire.hpp"
+
 #include <type_traits>
 #include <vector>
 #include <utility>
@@ -45,6 +50,15 @@ using fArray2 = Eigen::Array<floatType, 2, 1>;
 
 using fVector3 = Eigen::Matrix<floatType, 3, 1>;
 using fVector2 = Eigen::Matrix<floatType, 2, 1>;
+
+
+template<int DIM, typename T>
+class ArrayND;
+
+using Array1D = ArrayND<1, floatType>;
+using Array2D = ArrayND<2, floatType>;
+using Array3D = ArrayND<3, floatType>;
+
 
 
 // --------------------------------------------------------- Enums -------------------------------------------------------- //
@@ -178,6 +192,165 @@ enum class MultigridCycleType {
 };
 
 
+// ------------------------------------------------------ ArrayND Class --------------------------------------------------- //
+
+
+namespace CAMIRA_INTERNAL 
+{
+
+    // Structs that are specialised to hold permutation array for RAJA::Views. These need to be std::arrays, 
+    // but do not want to make them members of the ArrayND class.
+    template<int>
+    struct ViewPermutation;
+
+    template<>
+    struct ViewPermutation<1>
+    { static constexpr std::array< intType, 1 > perm{{0}}; };
+
+    template<>
+    struct ViewPermutation<2>
+    { static constexpr std::array< intType, 2 > perm{{1, 0}}; };
+
+    template<>
+    struct ViewPermutation<3>
+    { static constexpr std::array< intType, 3 > perm{{2, 1, 0}}; };
+
+}   // end namespace CAMIRA_INTERNAL
+
+
+// Class that wraps a RAJA::View and allocates its own memory to either DEVICE or HOST using umpire. 
+template<int DIM, typename T>
+class ArrayND {
+
+static_assert( DIM <= 3, "ArrayND dimensions must be 3 or less." );
+
+using ViewType = RAJA::View<T, RAJA::Layout<2, intType, 0>>;
+
+private:
+
+    floatType* m_data;
+    int m_allocatorId;  // Only hold the allocator id, not the allocator itself since it is not safe to use in device code
+    ViewType m_view;
+
+public:
+
+    template<typename... Args>
+    ArrayND( std::string resource,
+             Args... dim_sizes) :
+        m_data( nullptr ),
+        m_allocatorId( -1 ),
+        m_view( m_data, RAJA::make_permuted_layout( {{dim_sizes...}}, 
+                                                    CAMIRA_INTERNAL::ViewPermutation<DIM>::perm ) )    // Need to set dimensions of view at construction
+    {
+        static_assert( sizeof...(Args) == DIM, "Incorrect number of indices in ArrayND." );
+
+        if ( resource != "HOST" && resource != "DEVICE" ) {
+            throw std::runtime_error( "Resource for ArrayND must either be \"HOST\" or \"DEVICE\"." );
+        }
+
+        // Umpire allocator
+        auto &rm = umpire::ResourceManager::getInstance();
+        auto allocator = rm.getAllocator( resource ); 
+        m_allocatorId = allocator.getId();
+
+        // Allocate memory
+        intType size = (dim_sizes * ...);
+        m_data = static_cast<T*>( allocator.allocate( size * sizeof(T) ) );
+
+        // Reset view pointer
+        m_view.set_data( m_data );
+    }
+
+
+    ArrayND()                             = default;
+    ArrayND( ArrayND const & )            = default;
+    ArrayND( ArrayND && )                 = default;
+    ArrayND &operator=( ArrayND const & ) = default;
+    ArrayND &operator=( ArrayND && )      = default;
+    ~ArrayND()                            = default;
+
+
+    void CleanUp() {
+        if ( m_data ) {
+            auto &rm = umpire::ResourceManager::getInstance();
+            auto allocator = rm.getAllocator( m_allocatorId );
+            allocator.deallocate(m_data);
+            m_data = nullptr;
+        }     
+        m_view.set_data(m_data);
+    }
+
+
+    // Access the underlying view
+    RAJA_HOST_DEVICE
+    ViewType& view()
+    { return m_view; }
+
+    RAJA_HOST_DEVICE
+    ViewType& view() const
+    { return m_view; }
+
+
+    // Access to raw pointer
+    RAJA_HOST_DEVICE
+    T* data() const
+    { return static_cast<T*>( m_data ); }
+
+
+    // Total number of elements
+    RAJA_HOST_DEVICE
+    intType size() const 
+    { return m_view.size(); }
+
+
+    // Extent of a particular dimension
+    RAJA_HOST_DEVICE
+    intType dimension(intType dim) const { 
+        assert( (void("Multiarray dimension outside extents of array."), dim >= 0 && dim < DIM) );
+        return m_view.layout.extent(dim); 
+    }
+
+
+    // Multidimensional access
+    template<typename... Args>
+    RAJA_HOST_DEVICE
+    T& operator()(Args... indices) {
+        static_assert(sizeof...(Args) == DIM, "Incorrect number of indices in ArrayND.");
+        return m_view(indices...);
+    }
+
+    template<typename... Args>
+    RAJA_HOST_DEVICE
+    T& operator()(Args... indices) const {
+        static_assert(sizeof...(Args) == DIM, "Incorrect number of indices in ArrayND.");
+        return m_view(indices...);
+    }
+
+    // Copy underlying data from given array to this one
+    void CopyDataFrom( const ArrayND<DIM, T> &sourceArray ) const 
+    { 
+        auto &rm = umpire::ResourceManager::getInstance();
+        rm.copy( m_data, sourceArray.data() );  
+    }
+
+
+    // Set all elements to constant value - host only
+    void SetConstant( T value ) const {
+        auto &rm = umpire::ResourceManager::getInstance();
+        rm.memset( m_data, value );
+    }
+
+
+    // Set all elements to zero - host only
+    void SetZero() const {
+        static_assert( std::is_arithmetic<T>::value, "SetZero member function only supported for arithmetic types." );
+        auto &rm = umpire::ResourceManager::getInstance();
+        rm.memset( m_data, static_cast<T>(0) );
+    }
+
+};
+
+
 // ---------------------------------------------------- EnumVector Class -------------------------------------------------- //
 
 // Simple wrapper for c-style array that is safe to use in device code
@@ -198,20 +371,24 @@ struct EnumVector
 
 
     // Strong type indexing
+    RAJA_HOST_DEVICE
     constexpr T &operator[](const typename enumStruct::ENUMDATA idx)
     { return m_data[idx]; }
 
+    RAJA_HOST_DEVICE
     constexpr const T &operator[](const typename enumStruct::ENUMDATA idx) const 
     { return m_data[idx]; }
 
 
     // Only allowed when the object is holding axis data since the enum values should not change and are fairly standard
+    RAJA_HOST_DEVICE
     constexpr T &operator[](const size_t idx)
     { 
         static_assert( std::is_same<enumStruct, CAMIRA::Axis>::value, "EnumVector indexed with integral value only allowed for Axis enums" );
         return m_data[idx]; 
     }
 
+    RAJA_HOST_DEVICE
     constexpr const T &operator[](const size_t idx) const 
     { 
         static_assert( std::is_same<enumStruct, CAMIRA::Axis>::value, "EnumVector indexed with integral value only allowed for Axis enums" );
@@ -220,9 +397,11 @@ struct EnumVector
 
 
     // Get pointer to data
+    RAJA_HOST_DEVICE
     constexpr T* data()
     { return m_data; }
 
+    RAJA_HOST_DEVICE
     constexpr const T* data() const
     { return m_data; }
 
